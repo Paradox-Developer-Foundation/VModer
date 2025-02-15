2a90f342efe74300cee5f6f7f7c3b366a80e7889
textGamex <textGamex@outlook.com>
2025-02-15 23:39:03 +0800

    fix: 部分情况下无法正确识别字符串中的Icon部分

--- VModer.Core/Infrastructure/Parser/LocalizationFormatParser.cs
+++ VModer.Core/Infrastructure/Parser/LocalizationFormatParser.cs
@@ -28,7 +28,7 @@ public static class LocalizationFormatParser
         .Map(text => new LocalizationFormatInfo(string.Concat(text), LocalizationFormatType.Icon));
 
     private static readonly Parser<char, LocalizationFormatInfo> TextParser =
-        from text in Try(String("$$").WithResult('$')).Or(AnyCharExcept('$', '§')).AtLeastOnceString()
+        from text in Try(String("$$").WithResult('$')).Or(AnyCharExcept('$', '§', '£')).AtLeastOnceString()
         select new LocalizationFormatInfo(text, LocalizationFormatType.Text);
 
     private static readonly Parser<char, IEnumerable<LocalizationFormatInfo>> LocalizationTextParser =
