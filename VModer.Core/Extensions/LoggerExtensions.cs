a226dac9eb88dab57cda17247f5a3bcad07d20ce
textGamex <textGamex@outlook.com>
2025-03-11 00:12:49 +0800

    feat: 在日志中添加错误发生的位置信息

--- VModer.Core/Extensions/LoggerExtensions.cs
+++ VModer.Core/Extensions/LoggerExtensions.cs
@@ -5,8 +5,14 @@ namespace VModer.Core.Extensions;
 
 public static class LoggerExtensions
 {
-	public static void LogParseError(this Logger logger, ParserError error)
-	{
-		logger.Warn("文件解析失败, 原因: {Message}, path: {Path}", error.ErrorMessage, error.Filename);
-	}
+    public static void LogParseError(this Logger logger, ParserError error)
+    {
+        logger.Warn(
+            "文件解析失败, 原因: {Message}, path: {Path}, Line: {} Column: {}",
+            error.ErrorMessage,
+            error.Filename,
+            error.Line,
+            error.Column
+        );
+    }
 }
