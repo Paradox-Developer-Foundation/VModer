a4619cfe4d2987cbd6f8f71fd19e24bbb0c94f74
textGamex <textGamex@outlook.com>
2025-09-02 18:25:46 +0800

    build: 更新构建脚本, 简化操作

--- VModer.Core/build.ps1
+++ VModer.Core/build.ps1
@@ -1,3 +1,7 @@
+$serverOutputDir = "..\VModer.Extensions\client\server"
+
 dotnet publish -r win-x64 -o .\bin\publish\win-x64 ./VModer.Core.csproj
 dotnet publish -r linux-x64 -o .\bin\publish\linux-x64 ./VModer.Core.csproj
 dotnet publish -r osx-x64 -o .\bin\publish\osx-x64 ./VModer.Core.csproj
+Remove-Item -Path $serverOutputDir -Recurse -Force
+Copy-Item -Path .\bin\publish -Destination $serverOutputDir -Recurse
